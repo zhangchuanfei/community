fe6195ccb94f23b426d6eaf9b1b0bc68e18d7684
zhangchuanfei <394408146@qq.com>
2022-08-31 08:55:27 +0800

    database insert

--- src/main/java/chuanfei/community/community/controller/AuthorizeController.java
+++ src/main/java/chuanfei/community/community/controller/AuthorizeController.java
@@ -1,6 +1,8 @@
 package chuanfei.community.community.controller;
 import chuanfei.community.community.dto.AccessTokenDTO;
 import chuanfei.community.community.dto.GithubUser;
+import chuanfei.community.community.mapper.UserMapper;
+import chuanfei.community.community.model.User;
 import chuanfei.community.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,34 +10,56 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
+
 @Controller
 public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
-    @Value("${github.client.id")
+    @Value("${github.client.id}")
     private String clientId;
-    @Value("${github.client.secret")
+    @Value("${github.client.secret}")
     private String clientSecret;
-    @Value("${github.redirect.uri")
+    @Value("${github.redirect.uri}")
     private String redirectUri;
+
+    @Autowired
+    private UserMapper userMapper;
 
     @GetMapping("/callback")  //authorize后，Github会重定向回本站点，并返回code和state两个值
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state){
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request){
 
         //设置好获取access_token所需要的AccessTokenDTO
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         //code为Github传回，感觉应该是动态的
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setClient_id("e6766c1128a93a1e53ee");
-        accessTokenDTO.setClient_secret("7000c202f0e098e1c913032f91c2a0e3ff770100");
-        accessTokenDTO.setRedirect_uri("http://localhost:8888/callback");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setRedirect_uri(redirectUri);
         //根据Github提供的code获取accessToken
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        //根据access Token再次访问Github，获取user信息
         GithubUser githubUser = githubProvider.getUser(accessToken);
         System.out.println(githubUser.getName());
-        return "index";
+        if (githubUser != null){
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            //登录成功，写cookie和session
+            request.getSession().setAttribute("user", githubUser);
+            return "redirect:/";
+        }else{
+            //重新登录
+            return "redirect:/";
+        }
     }
 
 }
---
+++ src/main/java/chuanfei/community/community/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package chuanfei.community.community.mapper;
+
+import chuanfei.community.community.model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Component;
+
+@Mapper
+public interface UserMapper {
+    @Insert("insert into users (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
+    void insert(User user);
+
+}
